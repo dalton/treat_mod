75fdc083a2e6121a713dc0c8a1927d9e19fd9a4a
Adam Dalton <adalton@ihmc.us>
2014-05-30 15:00:05 -0400

    change stem to summary

--- lib/summarize.rb
+++ lib/summarize.rb
@@ -1,11 +1,12 @@
+require_relative 'treat/workers/extractors/summary'
+
 require 'treat'
 include Treat::Core::DSL
 
-require_relative 'treat/workers/extractors/summary'
 
 Treat::Workers::Extractors::Summary.add(:dummy) do |word, options={}|
   word.to_s[0..-2]
 end
 
-puts 'dummy'.stem(:dummy)     # => dumm
+puts 'dummy'.summary(:dummy)     # => dumm
 
